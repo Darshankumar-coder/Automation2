0f9f7f0a5a772e751286c851e96d5bb445168023
Darshankumar-coder <darshanr0512@gmail.com>
2025-04-06 14:33:08 +0530

    code working

---
+++ src/test/java/org/example/Samplerun.java
@@ -0,0 +1,16 @@
+package org.example;
+import org.testng.annotations.Test;
+import org.testng.Assert;
+
+public class Samplerun {
+    @Test
+    public void tests1()
+    {
+        Assert.assertTrue(true);
+    }
+    @Test
+    public void tests2()
+    {
+        Assert.assertTrue(true);
+    }
+}
